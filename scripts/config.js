97e8c25726d19b7822461639afcf998ee7a82d0d
asadbekdev07 <futureprotrader1007@gmail.com>
2024-01-22 13:17:55 +0500

    Fixed some issues, adding restart logic

--- scripts/config.js
+++ scripts/config.js
@@ -1,14 +1,12 @@
 function openPlayerConfig(event) {
-  editedPlayer = +event.target.dataset.playerid;
+  editedPlayer = +event.target.dataset.playerid; // +"1" => 1
   playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
-};
-
-
+}
 
 function closePlayerConfig() {
-  playerConfigOverlayElement.style.display = 'none';
-  backdropElement.style.display = 'none';
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
   errorsOutputElement.textContent = "";
   formElement.firstElementChild.lastElementChild.value = "";
@@ -17,18 +15,18 @@ function closePlayerConfig() {
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get("playername").trim();
+  const enteredPlayername = formData.get("playername").trim(); // "      " => ""
 
-  if (!enteredPlayername) {
+  if (!enteredPlayername) { // enteredPlayername === ""
     event.target.firstElementChild.classList.add("error");
     errorsOutputElement.textContent = "Iltimos ismingizni kiriting!";
     return;
   }
 
   const updatedPlayerDataElement = document.getElementById("player-" + editedPlayer + "-data");
-  updatedPlayerDataElement.children[1].textContent =enteredPlayername;
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
 
   players[editedPlayer - 1].name = enteredPlayername;
 
-  closePlayerConfig()
-};
+  closePlayerConfig();
+}
--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,33 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML = 'Siz gʻalaba qozondingiz, <span id="winner-name">OʻYINCHI NOMI</span>!';
+  gameOverElement.style.display = "none";
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = "";
+      gameBoardItemElement.classList.remove("disabled");
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (players[0].name === "" || players[1].name === "") {
-    alert("Iltimos ikkala o'yinchi nomini kiriting!")
+    alert("Iltimos ikkala o'yinchi ismini kiriting!");
     return;
   }
+
+  resetGameStatus();
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
-};
+}
 
 function switchPlayer() {
   if (activePlayer === 0) {
@@ -12,62 +35,95 @@ function switchPlayer() {
   } else {
     activePlayer = 0;
   }
-
   activePlayerNameElement.textContent = players[activePlayer].name;
-};
+}
 
-function selectGamefield(event) {
-  const selectedColumn = event.target.dataset.col - 1;
-  const selectedRow = event.target.dataset.row - 1;
-
-  if (gameData[selectedRow][selectedColumn] > 0) {
-    // alert("Iltimos boʻsh katakni tanlang!");
+function selectGameField(event) {
+  if (event.target.tagName !== "LI" || gameIsOver) {
     return;
   }
 
-  event.target.textContent = players[activePlayer].symbol;
-  event.target.classList.add("disabled");
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
 
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Iltimos boʻsh katakni tanlang!");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
-
   switchPlayer();
-};
+}
 
 function checkForGameOver() {
-  // Checking the rows for equalit
-  for (i = 0; i < 3; i++) {
+  // Checking the rows for equality
+  for (let i = 0; i < 3; i++) {
     if (
-      gameData[i][0] > 0 && gameData[i][0] === gameData[i][1] && gameData[i][1] == gameData[i][2]
-      ) {
-        return gameData[i][0];
-      };
-    };
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
 
-    for (i = 0; i < 3; i++) {
-      // Checking the columns for equalit
-      if (
-        gameData[0][i] > 0 && gameData[0][i] === gameData[1][i] && gameData[0][i] == gameData[2][i]
-        ) {
-          return gameData[0][i];
-        };
-      };
+  // Checking the columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
 
-      // Diagonal: top left to bottom right
-      if (
-        gameData[2][0] > 0 && gameData[2][0] === gameData[1][1] && gameData[1][1] === gameData[0][2]
-        ) {
-          return gameData[2][0]
-        };
+  // Diagonal: Top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
 
-        if(currentRound === 9) {
-          return -1;
-        }
+  // Diagonal: Bottom left to top right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
 
-        return 0;
-      };
+  if (currentRound === 9) {
+    return -1;
+  }
+
+  return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "Durrang";
+  }
+}
